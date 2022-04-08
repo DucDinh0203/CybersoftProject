c88cb7ae181883090870fd16d999db79b8479381
Duc Dinh <fxdinhtriduc@gmail.com>
2022-04-08 10:35:36 +1000

    edit index.jsx ShoeStore

---
+++ react-exercise/src/components/ShoesStore/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class ShoeStore
+ extends Component {
+  render() {
+    return (
+      <div>ShoeStore
+          
+      </div>
+    )
+  }
+}
